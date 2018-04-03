61ed59f98694cd1c4dacaf00201ce4a91de822a3
enzoFR60 <37971887+enzoFR60@users.noreply.github.com>
2018-04-03 16:51:35 +0200

    Update shared.lua

--- wallet/lua/weapons/wallet/shared.lua
+++ wallet/lua/weapons/wallet/shared.lua
@@ -1,54 +1,6 @@
-/*-----------------------------------------------------------------
-	Author		= SlownLS
-	Addon 		= Wallet
------------------------------------------------------------------*/
-
-AddCSLuaFile()
-
-SWEP.PrintName = "Porte-Monnaie"
-SWEP.Author = "SlownLS"
-SWEP.Category = "Other"
-SWEP.Instructions = ""
-
-SWEP.AnimPrefix  = ""
-SWEP.WorldModel = ""
-
-SWEP.Spawnable = true
-SWEP.AdminOnly = true
-SWEP.Category = "Other"
-SWEP.Primary.ClipSize = -1
-SWEP.Primary.DefaultClip = 0
-SWEP.Primary.Automatic = false
-SWEP.Primary.Ammo = ""
-
-SWEP.Secondary.ClipSize = -1
-SWEP.Secondary.DefaultClip = 0
-SWEP.Secondary.Automatic = false
-SWEP.Secondary.Ammo = ""
-
-function SWEP:Initialize()
-    self:SetHoldType( "normal")
-end
-
-function SWEP:Deploy()
-    return true
-end
-
-function SWEP:DrawWorldModel() end
-
-function SWEP:PreDrawViewModel(vm)
-    return true
-end
-
---[[-------------------------------------------------------------------------
-	Primary Attack
----------------------------------------------------------------------------]]
-
-function SWEP:PrimaryAttack()
-	self:SetNextPrimaryFire( CurTime() + 0.2 )
 -------------------------------------------------------------------
 --	Author		= SlownLS, 
---  Edit		= enzoFR60
+--      Edit		= enzoFR60
 --	Addon 		= Wallet
 -------------------------------------------------------------------
 
@@ -121,24 +73,5 @@ function SWEP:SecondaryAttack()
 		end
 		else
 		self.Owner:ChatPrint("Wallet is off" )
-	end
-end
-
-	if SERVER then
-		net.Start( "Wallet:Player:OpenMenu" )
-		net.Send( self.Owner )
-	end
-end
-
---[[-------------------------------------------------------------------------
-	Secondary Attack 
----------------------------------------------------------------------------]]
-
-function SWEP:SecondaryAttack()
-	self:SetNextPrimaryFire( CurTime() + 0.2 )
-
-	if SERVER then
-		net.Start( "Wallet:Player:OpenMenu" )
-		net.Send( self.Owner )
 	end
 end
